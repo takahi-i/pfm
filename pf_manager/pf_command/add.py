81268a44097bf9dfcf88b0e92c89c5bacdbe1984
takahi-i <takahi-i@cookpad.com>
2018-01-03 13:57:55 +0900

    Support login user

--- pf_manager/pf_command/add.py
+++ pf_manager/pf_command/add.py
@@ -11,15 +11,15 @@ class AddCommand(BaseCommand):
         self.config_path = config.obj["config"]
 
     def run(self):
-        forward_type, first_port, host, second_port, ssh_server = self.__parse(self.ssh_param_str)
+        forward_type, first_port, host, second_port, login_user, ssh_server = self.__parse(self.ssh_param_str)
 
         # add port forwarding target
         f = open(self.config_path, 'r')
         json_data = json.load(f)
         if forward_type == "L":
-            json_data[self.name] = {"type": forward_type, "remote_host":  host, "ssh_server": ssh_server, "local_port": first_port, "host_port": second_port}
+            json_data[self.name] = {"type": forward_type, "remote_host":  host, "ssh_server": ssh_server, "local_port": first_port, "host_port": second_port, "login_user" : login_user}
         elif forward_type == "R":
-            json_data[self.name] = {"type": forward_type, "remote_host":  host, "ssh_server": ssh_server, "server_port": first_port, "host_port": second_port}
+            json_data[self.name] = {"type": forward_type, "remote_host":  host, "ssh_server": ssh_server, "server_port": first_port, "host_port": second_port, "login_user" : login_user}
         else:
             raise RuntimeError("No type as " + forward_type)
         f.close()
@@ -33,10 +33,9 @@ class AddCommand(BaseCommand):
 
     def __parse(self, ssh_param_str):
         print("parsing:" + ssh_param_str)
-        m = re.match(r'^([RL]) ?(\d+):(.+):(\d+) (.+)$', ssh_param_str)
-        forward_type = m.group(1)
-        first_port = m.group(2)
-        host = m.group(3)
-        second_port = m.group(4)
-        ssh_server = m.group(5)
-        return forward_type, first_port, host, second_port, ssh_server
+        if ssh_param_str.count('@'):
+            m = re.match(r'^([RL]) ?(\d+):(.+):(\d+) +(.+)@(.+)$', ssh_param_str)
+            return m.group(1), m.group(2), m.group(3),m.group(4), m.group(5), m.group(6)
+        else:
+            m = re.match(r'^([RL]) ?(\d+):(.+):(\d+) +(.+)$', ssh_param_str)
+            return m.group(1), m.group(2), m.group(3),m.group(4), "", m.group(5)
--- pf_manager/pf_command/list.py
+++ pf_manager/pf_command/list.py
@@ -14,15 +14,15 @@ class ListCommand(BaseCommand):
         json_data = json.load(f)
         rows = self.convert_ditionary_to_2d_array(json_data)
         table = Texttable()
-        table.set_cols_align(["l", "l", "l", "l", "l", "l", "l"])
-        table.set_cols_width([20,  10,  10,  30,  10,  30,  12])
+        table.set_cols_align(["l", "l", "l", "l", "l", "l", "l", "l"])
+        table.set_cols_width([20,  10,  10,  30,  10,  15,  30,  12])
         table.add_rows(rows)
         print(table.draw())
         f.close()
         pass
 
     def convert_ditionary_to_2d_array(self, json_data):
-        header = ["name", "type", "local_port", "remote_host", "host_port", "ssh_server", "server_port"]
+        header = ["name", "type", "local_port", "remote_host", "host_port", "login_user", "ssh_server", "server_port"]
         body = []
         for name in json_data.keys():
             target = json_data[name]
