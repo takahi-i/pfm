aa96ea6d0aae818b13e089cfa99492a696e459a2
takahi-i <takahi-i@cookpad.com>
2018-01-03 18:37:31 +0900

    Make converter to util

--- pf_manager/pf_command/list.py
+++ pf_manager/pf_command/list.py
@@ -1,7 +1,9 @@
 import json
+
 from texttable import Texttable
 
 from pf_manager.pf_command.base import BaseCommand
+from pf_manager.util.util import convert_ditionary_to_2d_array
 
 
 class ListCommand(BaseCommand):
@@ -11,28 +13,10 @@ class ListCommand(BaseCommand):
     def run(self):
         f = open(self.config_path, 'r')
         json_data = json.load(f)
-        rows = self.convert_ditionary_to_2d_array(json_data)
+        rows = convert_ditionary_to_2d_array(json_data)
         table = Texttable()
         table.set_cols_align(["l", "l", "l", "l", "l", "l", "l", "l"])
         table.set_cols_width([20,  10,  10,  30,  10,  15,  30,  12])
         table.add_rows(rows)
         print(table.draw())
         f.close()
-        pass
-
-    def convert_ditionary_to_2d_array(self, json_data):
-        header = ["name", "type", "local_port", "remote_host", "host_port", "login_user", "ssh_server", "server_port"]
-        body = []
-        for name in json_data.keys():
-            target = json_data[name]
-            target_body = [name]
-            for field in header:
-                if field == "name":
-                    continue
-                if field in target:
-                    target_body.append(target[field])
-                else:
-                    target_body.append("")
-            body.append(target_body)
-        body.insert(0, header)
-        return body
---
+++ pf_manager/util/util.py
@@ -0,0 +1,16 @@
+def convert_ditionary_to_2d_array(json_data):
+    header = ["name", "type", "local_port", "remote_host", "host_port", "login_user", "ssh_server", "server_port"]
+    body = []
+    for name in json_data.keys():
+        target = json_data[name]
+        target_body = [name]
+        for field in header:
+            if field == "name":
+                continue
+            if field in target:
+                target_body.append(target[field])
+            else:
+                target_body.append("")
+        body.append(target_body)
+    body.insert(0, header)
+    return body
