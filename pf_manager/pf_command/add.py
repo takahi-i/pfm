61828922d19ed6cf567628a6770656691c3feb5c
takahi-i <takahikoito@gmail.com>
2018-01-03 10:45:19 +0900

    Implement AddCommand#run

--- pf_manager/pf_command/add.py
+++ pf_manager/pf_command/add.py
@@ -1,3 +1,4 @@
+import json
 import re
 from pf_manager.pf_command.base import BaseCommand
 
@@ -9,7 +10,24 @@ class AddCommand(BaseCommand):
         self.name = config.params["name"]
 
     def run(self):
-        self.__parse(self.ssh_param_str)
+        forward_type, first_port, host, second_port, ssh_server = self.__parse(self.ssh_param_str)
+
+        # add port forwarding target
+        f = open('.pfm', 'r')
+        json_data = json.load(f)
+        if forward_type == "L":
+            json_data[self.name] = {"type": forward_type, "remote_host":  host, "ssh_server": ssh_server, "local_port": first_port, "host_port": second_port}
+        elif forward_type == "R":
+            json_data[self.name] = {"type": forward_type, "remote_host":  host, "ssh_server": ssh_server, "server_port": first_port, "host_port": second_port}
+        else:
+            raise RuntimeError("No type as " + forward_type)
+        f.close()
+
+        # write the target
+        f = open('.pfm', 'w')
+        f.write(json.dumps(json_data, indent=4))
+        f.close()
+
 
     def __parse(self, ssh_param_str):
         print("parsing:" + ssh_param_str)
@@ -25,3 +43,5 @@ class AddCommand(BaseCommand):
         print("host: " + host)
         print("second_port: " + second_port)
         print("ssh_server:" + ssh_server)
+
+        return forward_type, first_port, host, second_port, ssh_server
--- pf_manager/pf_command/list.py
+++ pf_manager/pf_command/list.py
@@ -7,6 +7,6 @@ class ListCommand(BaseCommand):
     def run(self):
         f = open('.pfm', 'r')
         jsonData = json.load(f)
-        print(json.dumps(jsonData, sort_keys = True, indent = 4))
+        print(json.dumps(jsonData, sort_keys=True, indent=4))
         f.close()
         pass
