7ccd62a2548c65cd1e7de5cf3492f6d0993205c3
takahi-i <takahi-i@cookpad.com>
2018-01-03 19:00:25 +0900

    Add header list to dictionary convert function

--- pf_manager/pf_command/list.py
+++ pf_manager/pf_command/list.py
@@ -7,13 +7,16 @@ from pf_manager.util.util import convert_dictionary_to_2d_array
 
 
 class ListCommand(BaseCommand):
+
+    HEADERS = ["name", "type", "local_port", "remote_host", "host_port", "login_user", "ssh_server", "server_port"]
+
     def __init__(self, config):
         super(ListCommand, self).__init__(config)
 
     def run(self):
         f = open(self.config_path, 'r')
         json_data = json.load(f)
-        rows = convert_dictionary_to_2d_array(json_data)
+        rows = convert_dictionary_to_2d_array(json_data, ListCommand.HEADERS)
         table = Texttable()
         table.set_cols_align(["l", "l", "l", "l", "l", "l", "l", "l"])
         table.set_cols_width([20,  10,  10,  30,  10,  15,  30,  12])
--- pf_manager/util/util.py
+++ pf_manager/util/util.py
@@ -1,14 +1,13 @@
-def convert_dictionary_to_2d_array(json_data):
-    header = ["name", "type", "local_port", "remote_host", "host_port", "login_user", "ssh_server", "server_port"]
+def convert_dictionary_to_2d_array(json_data, headers):
     body = []
     for name in json_data.keys():
         target = json_data[name]
         target_body = []
-        for field in header:
+        for field in headers:
             if field in target:
                 target_body.append(target[field])
             else:
                 target_body.append("")
         body.append(target_body)
-    body.insert(0, header)
+    body.insert(0, headers)
     return body
