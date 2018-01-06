aabb72ee1171c0cffc6a91a3ea5e4e572238f2e1
takahi-i <takahiko03@gmail.com>
2018-01-07 00:03:29 +0900

    Capitalize first character of help messages

--- pf_manager/cli.py
+++ pf_manager/cli.py
@@ -28,16 +28,16 @@ def main(ctx, config):
         ctx.obj['config'] = ctx.params["config"]
 
 
-@main.command(help='add port forwarding target')
+@main.command(help='Add port forward setting')
 @click.pass_context
-@click.option('-n', '--name', type=str, help="name of port fowarding", required=False)
-@click.option('--forward-type', type=str, help="port forwarding type [L or R]", required=False, default='L')
-@click.option('--local-port', type=int, help="local port", required=False)
-@click.option('--remote-port', type=int, help="port of remote host", required=False)
-@click.option('--ssh-server', type=str, help="server to ssh login", required=False)
-@click.option('--server-port', type=int, help="server port", required=False)
-@click.option('--remote-host', type=str, help="remote host for port forwarding", required=False)
-@click.option('--login-user', type=str, help="login user of ssh server", required=False)
+@click.option('-n', '--name', type=str, help="Name of port fowarding", required=False)
+@click.option('--forward-type', type=str, help="Port forwarding type [L or R]", required=False, default='L')
+@click.option('--local-port', type=int, help="Local port", required=False)
+@click.option('--remote-port', type=int, help="Port of remote host", required=False)
+@click.option('--ssh-server', type=str, help="Server to ssh login", required=False)
+@click.option('--server-port', type=int, help="Server port", required=False)
+@click.option('--remote-host', type=str, help="Remote host for port forwarding", required=False)
+@click.option('--login-user', type=str, help="Login user of ssh server", required=False)
 @click.argument('ssh-argument', required=False)
 def add(ctx, name, forward_type, local_port, remote_port, ssh_server, server_port, remote_host, login_user, ssh_argument):
     try:
@@ -46,13 +46,13 @@ def add(ctx, name, forward_type, local_port, remote_port, ssh_server, server_por
         raise ClickException(error)
 
 
-@main.command(help='list existing port forwarding targets')
+@main.command(help='List existing port forward settings')
 @click.pass_context
 def list(ctx):
     ListCommand(ctx).run()
 
 
-@main.command(help='delete specified target')
+@main.command(help='Delete specified setting')
 @click.option('-n', '--name', type=str, help="name of port fowarding")
 @click.argument('name')
 @click.pass_context
@@ -60,7 +60,7 @@ def delete(ctx, name):
     DeleteCommand(ctx).run()
 
 
-@main.command(help='generate ssh port forward parameters')
+@main.command(help='Generate ssh port forward parameters')
 @click.argument('name')
 @click.pass_context
 def param(ctx, name):
