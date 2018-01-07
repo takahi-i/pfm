64ff10018df12adc19b60eff9964323156039d0e
takahi-i <takahiko03@gmail.com>
2018-01-07 23:00:40 +0900

    Skip create .pfm file when config path is set to None

--- pf_manager/pf_command/base.py
+++ pf_manager/pf_command/base.py
@@ -8,6 +8,11 @@ from pf_manager.util.log import logger
 class BaseCommand(object):
     def __init__(self, config_path):
         self.config_path = config_path
+
+        if self.config_path is None:
+            logger.info('config_path set to None...')
+            logger.info('Skip creating setting file...')
+
         if not os.path.exists(self.config_path):
             logger.info('Creating setting file of pfm in ' + self.config_path + '...')
             f = open(self.config_path, 'w')
