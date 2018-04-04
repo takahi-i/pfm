3e6eeb206635c29b8c8c9be7828e29a1cdf7c061
yoheikikuta <diracdiego@gmail.com>
2018-04-04 23:58:01 +0900

    Sort result table for visibility
    Sort result table by name for visibility.
    To do this, a function for sorting 2d array is added.
    Unit tests for utils functions are also added.

--- pf_manager/util/util.py
+++ pf_manager/util/util.py
@@ -1,3 +1,10 @@
+def create_ordered_2d_array_from_dict(json_data, headers):
+    rows = convert_dictionary_to_2d_array(json_data, headers)
+    rows = sort_body_order(rows)
+    rows = add_headers(rows, headers)
+    return rows
+
+
 def convert_dictionary_to_2d_array(json_data, headers):
     body = []
     for name in json_data.keys():
@@ -9,5 +16,14 @@ def convert_dictionary_to_2d_array(json_data, headers):
             else:
                 target_body.append("")
         body.append(target_body)
+    return body
+
+
+def sort_body_order(body):
+    body.sort(key=lambda x: x[0])
+    return body
+
+
+def add_headers(body, headers):
     body.insert(0, headers)
     return body
