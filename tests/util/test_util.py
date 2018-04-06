2d067d182a49bcad2faee58de34a28652af49fdc
yoheikikuta <diracdiego@gmail.com>
2018-04-07 01:57:37 +0900

    Add conditional branch for python2 and 3

--- tests/util/test_util.py
+++ tests/util/test_util.py
@@ -5,6 +5,9 @@ from pf_manager.util.util import convert_dictionary_to_2d_array
 from pf_manager.util.util import sort_body_order
 from pf_manager.util.util import add_headers
 
+import sys
+py_version = sys.version_info[0]
+
 HEADERS = ['name', 'local_port', 'login_user']
 JSON_DATA = {
         'b_test': {'local_port': '8889',
@@ -19,7 +22,10 @@ class TestUtil(unittest.TestCase):
     def test_create_oredered_2d_array_from_dict(self):
         expected = [['name', 'local_port', 'login_user'], ['a_test', '8888', None], ['b_test', '8889', None]]
         actual = create_ordered_2d_array_from_dict(JSON_DATA, HEADERS)
-        self.assertItemsEqual(expected, actual)
+        if py_version < 3:
+            self.assertItemsEqual(expected, actual)
+        else:
+            self.assertCountEqual(expected, actual)
 
     def test_convert_dictionary_to_2d_array(self):
         expected = [['b_test', '8889', None], ['a_test', '8888', None]]
