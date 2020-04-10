6b3ad455475669a9be4e64c03dfd6c3adfd64e42
takahiko.ito <takahiko.ito@flywheel.jp>
2020-04-11 01:11:21 +0900

    Fix installation failures

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@
 """The setup script."""
 import pip
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 LONG_DESCRIPTION = """
 Data scientists launch a Jupyter Notebook servers to tackle each machine learning task. Usually local computers are not
@@ -26,7 +30,7 @@ specifying the task name.
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ str(requirement.req) for requirement in parse_requirements('requirements.txt', session = pip.download.PipSession()) ]
+requirements = [ str(requirement.req) for requirement in parse_requirements('requirements.txt', session=False) ]
 
 setup_requirements = [
     # TODO(takahi-i): put setup requirements (distutils extensions, etc.) here
